7f79b83759e626c2370f76f73372fd4bb67bb6c5
leoska <leo77551@gmail.com>
2021-11-06 14:06:12 +0300

    first commit

---
+++ BSBO-0910-20-lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BSBO_0910_20_lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Определить в орграфе сильно связные компоненты, подсчитать их число и вывести состав (номера узлов) каждой сильно связной компоненты.
+            // Матрица инцидентности
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
